1a1a13b8426531bcf234692d3a60b0d9d62b986f
OMAR <rockstonestudios666@gmail.com>
2024-06-04 10:14:34 -0500

    without headers..

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -3,10 +3,7 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL: "https://alquelease-336066c2c468.herokuapp.com/api",
   withCredentials: true, // Incluir credenciales en las solicitudes
-  headers: {
-    'Access-Control-Allow-Origin': 'https://www.alquilease.com/', // Reemplazar con el dominio de tu frontend
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  }
+  
   
 });
 
