9bd01eaad78ce22b5435479156dd539ff51a665b
OMAR <rockstonestudios666@gmail.com>
2024-06-03 21:59:17 -0500

    ujuuuue

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: "https://alquelease-336066c2c468.herokuapp.com/api",
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
